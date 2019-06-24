343d415d5bd2a6d0ee54d90155e8f7bb59233c4f
GizmoTron64 <patrick_gray64@rocketmail.com>
2019-06-24 19:54:15 +0100

    Updated the FindReplace tool with all methods. Not fully working though

--- src/mutators/FindReplace.java
+++ src/mutators/FindReplace.java
@@ -9,67 +9,121 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class FindReplace {
-    private ArrayList<String> methods;
-    private String mutator;
-    private String output;
-    private File inputFile;
+    private String mutation;
+    private String method;
+    private String operator;
     private Charset charset;
     private String content;
+    private ArrayList<String> rtxcMethods;
+    private ArrayList<String> rcxcMethods;
+    private ArrayList<String> mxtMethods;
+    private ArrayList<String> mspMethods;
+    private ArrayList<String> espMethods;
+    private ArrayList<String> operatorList;
+
 
 
     public FindReplace(String file) throws IOException{
-         methods = new ArrayList<String>(Arrays.asList("wait", "sleep", "join", "await", "lock", "unlock", "signal",
-                                                        "signalAll", "acquire", "release", "countDown", "submit"));
-         mutator = ""; // Default replacement is a removal
-         output = "";
-         inputFile = new File(file);
+         operatorList = new ArrayList<String>(Arrays.asList("rtxc", "rcxc", "mxt", "msp", "esp"));
+         rtxcMethods = new ArrayList<String>(Arrays.asList("wait", "join", "sleep", "notify", "notifyAll"));
+         rcxcMethods = new ArrayList<String>(Arrays.asList("lock", "unlock", "signal", "signalAll", "acquire", "release", "countDown", "submit"));
+         mxtMethods = new ArrayList<String>(Arrays.asList("wait", "sleep", "join", "await"));
+         mspMethods = new ArrayList<String>(Arrays.asList("synchronized"));
+         espMethods = new ArrayList<String>(Arrays.asList("synchronized"));
+         method = null;
+         mutation = null;
+         operator = null;
+         File inputFile = new File(file);
          charset = StandardCharsets.UTF_8;
          Path path = Paths.get(inputFile.getAbsolutePath());
          content = new String(Files.readAllBytes(path), charset);
-
-
     }
 
-    public String getMutator() {
-        return mutator;
+    public String getMutation() {
+        return mutation;
     }
 
-    public void setMutator(String r) {
-        mutator = r;
+    public void setMutation(String r) {
+        mutation = r;
     }
 
-    public ArrayList<String> getMethods() {
-        return methods;
+    public void setMethod(ArrayList<String> methodList) {
+        method = methodList.get(new Random().nextInt(methodList.size()));
     }
 
-    public void setInputFile(String filename) {
-        inputFile = new File(filename);
+    public void setMethod(ArrayList<String> methodList, String method) {
+        if(methodList.contains(method)) {
+            this.method = method;
+        } else {
+            throw new IllegalArgumentException("Method not found");
+        }
     }
 
-    public void addMethod(String m) {
-        methods.add(m);
+    public ArrayList<String> setOperator(String operator) {
+        if(operatorList.contains(operator)) {
+            this.operator = operator;
+
+            if(operator.equals("rtxc")){
+                mutation = "";
+                setMethod(rtxcMethods);
+                return rtxcMethods;
+            } else if(operator.equals("rcxc")) {
+                mutation = "";
+                setMethod(rcxcMethods);
+                return rcxcMethods;
+            } else if(operator.equals("mxt")) {
+                mutation = "*2";
+                setMethod(mxtMethods);
+                return mxtMethods;
+            } else if(operator.equals("msp")) {
+                setMethod(mspMethods);
+                return mspMethods;
+            } else if(operator.equals("esp")) {
+                setMethod(espMethods);
+                return espMethods;
+            }
+        } else {
+            throw new IllegalArgumentException("Operator not found");
+        }
+        return null;
     }
 
+    public void setOperator(String operator, String method) {
+        setMethod(setOperator(operator), method);
+    }
 
-    public void rep() throws IOException {
-        String regex = "(wait\\(\\w+)(\\)\\;)";
+    public String getOperator() {
+        return operator;
+    }
+
+    public void replaceMutation() throws IOException {
+        String regex = "(" + method + "\\(\\w*/)(\\)\\;)"; //"(wait\\(\\w*/)(\\)\\;)"; //
         Pattern p = Pattern.compile(regex);
         Matcher m = p.matcher(content);
+        System.out.println("no match");
 
         while(m.find()) {
+            System.out.println("match");
             String replacement;
-            if(mutator != "") {
-                replacement = m.group(1) + mutator + m.group(2); //"wait(" + m.group(1) + replacement;
+            if(!mutation.equals("")) {
+                replacement = m.group(1) + mutation + m.group(2); //"wait(" + m.group(1) + replacement;
+                System.out.println("Here");
             } else {
-                replacement = mutator;
+                replacement = mutation;
+                System.out.println("Here lo");
             }
             content = content.replaceFirst(regex, replacement);
         }
-
+        System.out.println("Operator: " + operator +
+                            "\nMutation: " + mutation +
+                            "\nMethod: " + method +
+                            "\nRegex: " + regex +
+                            "\nContent: " + content);
         writeToFile();
 
     }
--- src/mutators/test.java
+++ src/mutators/test.java
@@ -6,8 +6,7 @@ public class test {
 
     public static void main(String[] args) throws IOException {
         FindReplace fr = new FindReplace("H:\\My Documents\\Dissertation\\test1.txt"); //"C:\\Users\\headl\\OneDrive\\Documents\\Uni\\Dissertation\\test1.txt"
-        fr.addMethod("wait");
-        fr.setMutator("*2");
-        fr.rep();
+        fr.setOperator("mxt", "wait");
+        fr.replaceMutation();
     }
 }
